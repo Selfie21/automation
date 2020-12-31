6a7ab43631e59ed93c9947a33a9734bf0ef28dcd
Selfie <pierre.brosemer@gmail.com>
2020-12-31 16:50:30 +0100

    final version

--- autoclicker.py
+++ autoclicker.py
@@ -6,14 +6,13 @@ import random
 #Set Positions/Colours
 color_grey = [43, 47, 54]
 color_yellow = [240, 185, 11]
-color_green = [10, 181, 107]
-color_red = [164, 70, 88]
-
-base_again = [1420, 445]
+color_yellow_two = [248, 209, 47]
+base_again = [1385, 445]
+base_end = [1420, 445]
 base_cnt = [1205, 301]
 base_long = [1140, 475]
 base_short = [1490, 475]
-base_result = [1544, 796]
+base_result = [1544, 797]
 
 
 #Click Stuff
@@ -29,7 +28,7 @@ def click_startup_routine(randomizer_time):
 		short_click(base_short)
 	else:
 		short_click(base_long)
-	time.sleep(11)
+	time.sleep(13)
 
 def short_click(positions):
 	pyautogui.click(positions[0], positions[1])
@@ -42,9 +41,9 @@ def check_matching_array(arr1, arr2):
 def get_colour(coords):
 	return pyautogui.pixel(coords[0],coords[1])
 
-def check_for_colour_change(coords, colour):
-	while not check_matching_array(get_colour(coords), colour):
-		time.sleep(0.05)
+def check_for_colour_change(coords):
+	while not ((get_colour(coords)[1] > 100) and (get_colour(coords)[1] < 200)):
+		time.sleep(0.01)
 	return True
 	
 
@@ -53,34 +52,25 @@ def handler(signum, frame):
 	raise Exception("Timeout")
 
 
-
-
 while True:
 	randomizer_time = random.randint(0,15)
 	current_color = get_colour(base_again)
-		
-	if check_matching_array(current_color, color_yellow):
+	print("Calling Startup Routine;")
+	if check_matching_array(current_color, color_yellow) or check_matching_array(current_color, color_yellow_two):
 		randomizer_short = random.randint(1, 3)
-		randomizer_position = random.randint(0,10)
 		click_startup_routine(randomizer_short)
 	
 	
 	#ending Battle
 	signal.signal(signal.SIGALRM, handler)
-	signal.alarm(10)
+	signal.alarm(300)
 
 	try:
-		if check_for_colour_change(base_result, color_green):
-			print("I found green clicking now! Click routine can continue")
-			short_click(base_again)
+		if check_for_colour_change(base_result):
+			print("I found green clicking now! Click routine can continue;")
+			short_click(base_end)
 	except Exception: 
-		print("TIMEOUT OCCURED")
+		print("Timeout Occurred;")
 	signal.alarm(0)
 	
 	time.sleep(10 + randomizer_time)
-	
-
-
-
-
-
